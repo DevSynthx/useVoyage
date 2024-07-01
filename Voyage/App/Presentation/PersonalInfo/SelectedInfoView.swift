303735cdfd2ec1e57658183e292d3f337fe107e3
DavidEtoedia <etoedia@gmail.com>
2024-07-01 10:07:43 -0700

    fix: font size and selected item color

---
+++ Voyage/App/Presentation/PersonalInfo/SelectedInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectedInfoView.swift
+//  Voyage
+//
+//  Created by Inyene Etoedia on 30/06/2024.
+//
+
+import SwiftUI
+
+struct SelectedInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectedInfoView()
+}
