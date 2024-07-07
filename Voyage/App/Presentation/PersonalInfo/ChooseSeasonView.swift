477992d3f81a757308ed6db2f4bdb9d4dadbe07f
DavidEtoedia <etoedia@gmail.com>
2024-07-07 14:37:54 -0700

    feat: choose season view

---
+++ Voyage/App/Presentation/PersonalInfo/ChooseSeasonView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseSeasonView.swift
+//  Voyage
+//
+//  Created by Inyene Etoedia on 04/07/2024.
+//
+
+import SwiftUI
+
+struct ChooseSeasonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseSeasonView()
+}
