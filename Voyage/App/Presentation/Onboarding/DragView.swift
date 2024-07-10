23948f4b5249d7152ff5340aeec240d87d6e9490
DavidEtoedia <etoedia@gmail.com>
2024-07-10 03:45:46 -0700

    feat: onboarding screen

---
+++ Voyage/App/Presentation/Onboarding/DragView.swift
@@ -0,0 +1,18 @@
+//
+//  DragView.swift
+//  Voyage
+//
+//  Created by Inyene Etoedia on 09/07/2024.
+//
+
+import SwiftUI
+
+struct DragView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragView()
+}
